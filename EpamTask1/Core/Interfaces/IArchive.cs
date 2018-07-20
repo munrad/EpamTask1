b141d200fae8521d6a1e14ca8102e482722f53eb
Denis Bazhenov <denis_bazhenov@epam.com>
2018-07-20 23:58:20 +0400

    Add first for Task 4

---
+++ EpamTask1/Core/Interfaces/IArchive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpamTask1.Core.Interfaces
+{
+    interface IArchive
+    {
+        IUser User { get; set; }
+        IBook Book { get; set; }
+        IPaper Paper { get; set; }
+        string[] Journal { get; set; }
+        DateTime DateGet { get; set; }
+        DateTime DateReturn { get; set; }
+        int DateLimit { get; set; }
+    }
+}
---
+++ EpamTask1/Core/Interfaces/IUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EpamTask1.Core.Interfaces
+{
+    interface IUser
+    {
+        string Name { get; set; }
+        int NumTicket { get; set; }
+        DateTime DateReg { get; set; }
+        int PhoneNum { get; set; }
+        bool isBlock { get; set; }
+        string City { get; set; }
+        IBook Book { get; set; }
+        IPaper Paper { get; set; }
+        IPatent Patent { get; set; }
+    }
+}
