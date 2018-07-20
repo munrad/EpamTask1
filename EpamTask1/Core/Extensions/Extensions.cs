981293fbbd299be9f09f519529b247c1a40a001e
Denis Bazhenov <denis_bazhenov@epam.com>
2018-07-20 12:10:31 +0400

    Add IsNotNull class

---
+++ EpamTask1/Core/Extensions/Extensions.cs
@@ -0,0 +1,58 @@
+﻿using EpamTask1.Core.Interfaces.Catalog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EpamTask1.Core.Classes;
+using EpamTask1.Core.Interfaces;
+
+namespace EpamTask1.Core.Extensions
+{
+    internal class Extensions
+    {
+        public static void Deserialize(ref List<ICatalogObject> obj, List<string> file)
+        {
+            try
+            {
+                foreach (var item in file)
+                {
+                    var tmp = item.Split('/');
+                    var tmp1 = tmp[1].Split('>');
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        public static void Serializer<T>(ref List<string> list, T obj)
+        {
+            var type = typeof(T);
+            var prop = type.GetProperties();
+            var str = $"{type.FullName}/";
+            str = prop.Aggregate(str, (current, propertyInfo) =>
+            {
+                var result = current + $"{propertyInfo.Name}:";
+                if (propertyInfo.PropertyType == typeof(List<string>))
+                {
+                    var propVal = string.Empty;
+                    if (propertyInfo.GetValue(obj, null) is List<string> listTmp)
+                    {
+                        propVal = listTmp.Cast<object>().Aggregate(propVal, (current1, n) => current1 + $"{n}|");
+                        propVal = propVal.Remove(propVal.LastIndexOf('|'), 1);
+                    }
+                    result += $"{propVal}>";
+                }
+                else
+                {
+                    result += $"{propertyInfo.GetValue(obj)}>";
+                }
+                return result;
+            });
+            str = str.Remove(str.LastIndexOf('>'), 1);
+            list.Add(str);
+        }
+    }
+}
---
+++ EpamTask1/Core/Extensions/Validator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using EpamTask1.Core.Attributes;
+using EpamTask1.Core.Classes;
+using EpamTask1.Core.Interfaces;
+
+namespace EpamTask1.Core.Extensions
+{
+    public class Validator
+    {
+        public static void ValidateProp<T>(T myObject)
+        {
+            var prop = myObject.GetType().GetProperties();
+            foreach (var propertyInfo in prop)
+            {
+                var attr = propertyInfo.CustomAttributes;
+                if (attr.Any(m => m.AttributeType == typeof(IsNotNullOrEmpty)))
+                    IsAnyNullOrEmpty(myObject);
+            }
+        }
+
+        private static bool IsAnyNullOrEmpty(object myObject)
+        {
+            return myObject.GetType().GetProperties()
+                .Where(pi => pi.GetValue(myObject) is string)
+                .Select(pi => (string)pi.GetValue(myObject))
+                .Any(string.IsNullOrEmpty);
+        }
+    }
+}
